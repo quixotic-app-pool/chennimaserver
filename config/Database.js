912663d665e3b6aaf2e06fb03c17540365957c02
Michael Chen <maochen201411@gmail.com>
2017-10-06 11:28:37 +0800

    bugfix

--- config/Database.js
+++ config/Database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    // url : 'mongodb://myTester:xyz123@localhost:27017/test'
-    url : 'mongodb://localhost:27017'
+    url : 'mongodb://myTester:xyz123@localhost:27017/test'
+    // url : 'mongodb://localhost:27017'
 };
